c15d090754d972623148c2b266bbe63e7a5fb631
Bernard <hurgoon@gmail.com>
2018-12-14 10:45:52 +0900

    [허진성] 1. 리뷰 상세페이지 뷰 컨트롤러 만들었습니다.

---
+++ ShareProject/MangoPractice/MangoPractice/ViewController/ReviewDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReviewDetailViewController.swift
+//  MangoPractice
+//
+//  Created by Bernard Hur on 14/12/2018.
+//  Copyright © 2018 Bernard Hur. All rights reserved.
+//
+
+import UIKit
+
+class ReviewDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+
+}
