df391234f7d8a7cc4895dce4687b61e5b96d30f0
Jinsun <alfkzmf8805@gmail.com>
2018-12-14 15:45:00 +0900

    <진선> 1. 내정보 페이지 구매한 잇딜, 가고싶다, 내가 등록한 식당 UI셋팅 완료하였습니다. 로그인 구현 및 서버 구현되는 대로 기능 구현 이어나가겠습니다,

--- ShareProject/MangoPractice/MangoPractice/ViewController/PurchasedEatDealViewController.swift
+++ ShareProject/MangoPractice/MangoPractice/ViewController/PurchasedEatDealViewController.swift
@@ -7,24 +7,32 @@
 //
 
 import UIKit
+import SnapKit
 
 class PurchasedEatDealViewController: UIViewController {
     let topView = UIView()
     let backButton = UIButton()
     let titleLabel = UILabel()
+    var mainCollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+    var arrayOfCellData = CellDataOrigin().arrayOfCellData //임시로 하드코딩 데이터 삽입
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         topViewConfig()
+        mainCollectionViewConfig()
 
     }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+    
     private func topViewConfig() {
         
         view.addSubview(topView)
         topView.addSubview(backButton)
         topView.addSubview(titleLabel)
-        
-        view.backgroundColor = #colorLiteral(red: 0.913626194, green: 0.9137828946, blue: 0.9136161804, alpha: 1)
         
         topView.backgroundColor = .white
         backButton.setImage(UIImage(named: "backArrowButton"), for: .normal)
@@ -34,6 +42,7 @@ class PurchasedEatDealViewController: UIViewController {
         titleLabel.backgroundColor = .clear
         titleLabel.textColor = #colorLiteral(red: 1, green: 0.4456674457, blue: 0.004210381769, alpha: 1)
         backButton.addTarget(self, action: #selector(backButtonDidTap), for: .touchUpInside)
+        
         
         topView.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
@@ -58,5 +67,56 @@ class PurchasedEatDealViewController: UIViewController {
         dismiss(animated: true, completion: nil)
         
     }
+    
+    private func mainCollectionViewConfig() {
+        view.addSubview(mainCollectionView)
+        
+        mainCollectionView.backgroundColor = #colorLiteral(red: 0.913626194, green: 0.9137828946, blue: 0.9136161804, alpha: 1)
+        mainCollectionView.dataSource = self
+        mainCollectionView.delegate = self
+        mainCollectionView.register(PurchasedEatDealCollectionViewCell.self, forCellWithReuseIdentifier: "CELL")
+        
+        mainCollectionView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalTo(topView.snp.bottom)
+        }
+        
+    }
+}
+extension PurchasedEatDealViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 100)
+    }
+    // 셀간 간격 주기
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    // 맨 위랑 아래 살짝 떼기
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 1, left: 0, bottom: 1, right:0)
+    }
 
+}
+
+extension PurchasedEatDealViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arrayOfCellData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = mainCollectionView.dequeueReusableCell(withReuseIdentifier: "CELL", for: indexPath) as! PurchasedEatDealCollectionViewCell
+        cell.imageView.image = UIImage(named: "blur-breakfast-close-up-376464") //임시 디폴트 이미지 삽입
+        cell.subNameLabel.text = "\(arrayOfCellData[indexPath.row].address)"
+        cell.nameLabel.text = "\(arrayOfCellData[indexPath.row].name)"
+        cell.priceLabel.text = "\(arrayOfCellData[indexPath.row].gradePoint)"
+        cell.dateLabel.text = "2018-12-21~2019-03-07"
+        
+        return cell
+    }
+    
+    
+}
+
+extension PurchasedEatDealViewController: UICollectionViewDelegate {
+    
 }
